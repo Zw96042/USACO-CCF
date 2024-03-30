a3b5f20103ffebd79029cf3a690ccffc5eec4da1
Zachary Wilson <zw96042@gmail.com>
2024-03-29 19:15:06 -0500

    #1 Bronze 2022

---
+++ #3-Bronze-December/main.py
@@ -0,0 +1,9 @@
+"""
+N plants are growing at different rates.
+
+Find the minimum amount of time required so that the
+height of each plant follows a given ordering
+
+Print -1 if the ordering is impossible to achieve
+"""
+
---
+++ 2022/#1-Bronze-December/main.py
@@ -0,0 +1,18 @@
+N = int(input())
+C = [int(i) for i in input().split()]
+
+# Find least possible amount of tuition
+revenue = min(C)
+tuition = 1000000
+
+C.sort()
+
+for i in range(N-1):
+    revenue = max((N - i) * C[i], revenue)
+    
+for i in range(N-1):
+    if revenue == (N - i) * C[i]:
+        tuition = min(C[i], tuition)
+    
+print(revenue, tuition)
+# print(tuition2)
