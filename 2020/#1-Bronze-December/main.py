a10672691e3fac57c20a8a69032e03b7d604368a
Zachary Wilson <zw96042@gmail.com>
2024-04-01 20:06:48 -0500

    Comments

--- 2020/#1-Bronze-December/main.py
+++ 2020/#1-Bronze-December/main.py
@@ -1,14 +1,12 @@
 S = [int(i) for i in input().split()]
 
-ABC = max(S) 
-S.remove(ABC)
+ABC = max(S) # The sum of A, B, and C will always be the highest integer
 
-A = min(S) 
-S.remove(A)
+A = min(S) # A is ALWAYS the least inclusive value
+S.remove(A) # Remove A to find a different minimum aside from A
 
-B = min(S) 
-S.remove(B)
+B = min(S) # B is ALWAYS the 2nd lowest value aside from A
 
-C = ABC - A - B
+C = ABC - A - B # Calculate C using sum of ABC, A, and B
 
-print(A, B, C)
+print(A, B, C) # Print in correct format
